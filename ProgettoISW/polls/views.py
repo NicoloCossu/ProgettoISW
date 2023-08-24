e16cb758cc066690b9f1468ae74dbcdb2763a4fb
Neune0 <raimondo.masala.01001@gmail.com>
2023-08-24 16:51:03 +0200

    test db finto

--- ProgettoISW/polls/views.py
+++ ProgettoISW/polls/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "polls/home.html",{})
+    animals= [
+        {'specie':'ciao', 'razza': 'aaa', 'eta': 'tt','descrizione': 'd'},
+        {'specie':'ciao2', 'razza': 'aaa', 'eta': 'tt','descrizione': 'd'},
+        {'specie':'ciao3', 'razza': 'aaa', 'eta': 'tt','descrizione': 'd'},
+    ]
+    return render(request, "polls/home.html",{'animals':animals})
