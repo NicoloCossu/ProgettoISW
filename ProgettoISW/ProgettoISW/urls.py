3276ad65d9946ae64a67a410e6234155ebe44324
Neune0 <raimondo.masala.01001@gmail.com>
2023-08-26 16:26:25 +0200

    views register e login v1

--- ProgettoISW/ProgettoISW/urls.py
+++ ProgettoISW/ProgettoISW/urls.py
@@ -20,4 +20,6 @@ from polls import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="home"),
+    path('register',views.registerPage, name='register'),
+    path('login',views.loginPage, name='login')
 ]
--- ProgettoISW/polls/views.py
+++ ProgettoISW/polls/views.py
@@ -7,3 +7,18 @@ def home(request):
         {'specie':'ciao3', 'razza': 'aaa', 'eta': 'tt','descrizione': 'd'},
     ]
     return render(request, "home.html",{'animals':animals})
+
+
+def registerPage(request):
+    form= UserCreationForm()
+
+    if request.method == 'POST':
+        form= UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context={'form':form}
+    return render(request, 'polls/register.html',context)
+
+def loginPage(request):
+    context={}
+    return render(request, 'polls/login.html',context)
