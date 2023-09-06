17495c02faba2e0268f97c6a24620704573290e2
Entity534 <smulinelli@gmail.com>
2023-09-06 17:24:27 +0200

    Sistemati nomi dei campi nel modulo di adozione
    Sistemato anche il pulsante home

--- ProgettoISW/polls/forms.py
+++ ProgettoISW/polls/forms.py
@@ -5,3 +5,8 @@ class RichiestaAdozioneForm(forms.ModelForm):
     class Meta:
         model = RichiestaAdozione
         fields = ['nomeCognome', 'indirizzo', 'emailNumeroDiTelefono']
+        labels = {
+            'nomeCognome': 'Nome e Cognome',
+            'indirizzo': 'Indirizzo',
+            'emailNumeroDiTelefono': 'Email o Numero di Telefono',
+        }
